1ef8f062814dbb0144a0e15520d3072ec27b8e87
TranPhamMinhDuc <doggygerman@gmail.com>
2025-06-24 12:10:50 +0700

    final(24-6-25)
    final theo ngày

---
+++ Levels/Scripts/Save/SaveSystem.cs
@@ -0,0 +1,42 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SaveSystem
+{
+    public static void SaveGame(string saveName)
+    {
+        SaveData data = new SaveData
+        {
+            saveName = saveName,
+            playerHealth = GameData.Instance.playerHealth,
+            playerScore = GameData.Instance.playerScore,
+            playerPosition = new Vector3Serializable(GameData.Instance.playerPosition),
+            sceneName = SceneManager.GetActiveScene().name
+        };
+
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(GetPath(saveName), json);
+    }
+
+    public static SaveData LoadGame(string saveName)
+    {
+        string path = GetPath(saveName);
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            return JsonUtility.FromJson<SaveData>(json);
+        }
+        return null;
+    }
+
+    public static string[] GetAllSaveFiles()
+    {
+        return Directory.GetFiles(Application.persistentDataPath, "*.json");
+    }
+
+    public static string GetPath(string saveName)
+    {
+        return Path.Combine(Application.persistentDataPath, saveName + ".json");
+    }
+}
